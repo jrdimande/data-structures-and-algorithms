5f8d2ccd0501ec3876489383b7af7be28681f598
jrdimande <timoteojrdimande@gmail.com>
2025-08-27 23:42:11 +0200

    Recursive counting

---
+++ recursion/CountUntilZero.java
@@ -0,0 +1,16 @@
+package recursion;
+
+public class CountUntilZero {
+    public static void count(int n){
+        if (n == 0){
+            System.out.println("Finished");
+        }else {
+            System.out.println(n);
+            count(n - 1);
+        }
+    }
+
+    public static void main(String[] args){
+        count(10);
+    }
+}
