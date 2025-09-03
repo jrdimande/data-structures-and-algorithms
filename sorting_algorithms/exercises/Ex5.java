7fd35a2a6fb6c8264ec7d33ecfe7b29ba938c8c4
jrdimande <timoteojrdimande@gmail.com>
2025-09-04 01:37:47 +0200

    Exercise 5

---
+++ sorting_algorithms/exercises/Ex5.java
@@ -0,0 +1,28 @@
+package sorting_algorithms.exercises;
+import java.util.Arrays;
+
+public class Ex5 {
+
+    public static void lengthBasedStringSort(String[] array) {
+        int len = array.length;
+
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - 1 - i; j++) {
+                if (array[j].length() > array[j + 1].length()) {
+                    // troca
+                    String temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"uva", "banana", "maca", "kiwi"};
+
+        System.out.println("Antes de ordenar: " + Arrays.toString(names));
+        lengthBasedStringSort(names);
+        System.out.println("Depois de ordenar: " + Arrays.toString(names));
+    }
+}
