387b39363c86c2929b197c36753081a4a2c7cdc7
jrdimande <timoteojrdimande@gmail.com>
2025-10-26 17:56:43 +0200

    ...

---
+++ data_structure/doubly_linkedlist/Node.java
@@ -0,0 +1,16 @@
+package data_structure.doubly_linkedlist;
+
+public class Node {
+    int data;
+    Node prev;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.prev = null;
+        this.next = null;
+
+    }
+
+
+}
